2629ca2a2191e91a54e3e4c5f01502081114d358
Yunus Almuhajiri <yunusmutashimbillah@gmail.com>
2018-08-23 14:02:40 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Yunus Mutashimbillah Almuhajiri</Text>
+        <Text>XI RPL 4 / 36</Text>
+        <Text>SMK Telkom Purwokerto</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f9f9f9',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
